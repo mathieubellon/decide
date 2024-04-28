5d8d5ac1b7196f6c4cf21470ae53a7d7fa7c0a00
Mathieu <contact@mathieubellon.fr>
2024-04-28 11:28:24 +0200

    Add helmet middleware for enhanced security

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
@@ -33,7 +34,9 @@ func main() {
 	app.Use(logger.New())
 	app.Use(csrf.New())
 	app.Use(recover.New())
-	app.Use(Protect)
+	app.Use(helmet.New())
+	// 404 Handler
+
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://127.0.0.1:8000, http://localhost:5173",
 		AllowHeaders:     "Origin, Content-Type, Accept",
@@ -52,10 +55,15 @@ func main() {
 	app.Get("/logout", Logout)
 
 	api := app.Group("/api") // /api
+	api.Use(Protected)
 	api.Get("/me", Me)
 	v1 := api.Group("/v1") // /api/v1
 	v1.Get("/ideas", ListIdeas)
 
+	// Catch All route for 404 : TODO : handle this frontend only ?
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
 	// data, _ := json.MarshalIndent(app.GetRoutes(true), "", "  ")
 	// fmt.Print(string(data))
 
